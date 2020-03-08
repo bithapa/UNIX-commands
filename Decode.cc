3f2dbe73f3f48b4e9df8fcd7f8f2b51dd28b5d82
Bikash Thapa <bikashthapa.np@gmail.com>
2020-03-08 00:10:29 -0500

    decrypted

--- Decode.cc
+++ Decode.cc
@@ -16,21 +16,37 @@ struct Encoded {
 int main () {
 	ifstream file ("coded_message.txt", ios::in | ios::binary);
 	if ( file.is_open() ) {
-		streampos begin, end, size;
-		
-		Encoded encoded_data;
-		file.seekg(0, ios::beg);
-		file.read((char *) &encoded_data, sizeof(Encoded));
+		// compute the size of whole bin. file == end - begin
+		streampos begin, end;
+  		begin = file.tellg();
+  		file.seekg (0, ios::end);
+  		end = file.tellg();
+		// number of encrypted 'Encoded' structs
+		int total_encoded = (end - begin) / sizeof(Encoded);
+		cout << "Total Messages:\t" << total_encoded << endl;
+		// set the cursor at the beginning of bin. file
+		file.seekg (0, ios::beg);
 
-		int key = encoded_data.key;
-		cout << "Key: " << key << endl;
-		cout << "Message: " << endl; 
-		for (int i = 0; i < BUFFER_SIZE; i++){
-			cout << (char)(encoded_data.message[i]-key-i) << endl;
+		Encoded encoded_data[total_encoded];
+		for ( int i=0; i < total_encoded; i++) {
+			file.read((char *) &encoded_data[i], sizeof(Encoded));
 		}
-		
+
+		int key = 0;
+		for ( int i=0; i < total_encoded; i++) {
+			key = encoded_data[i].key;
+			cout << "Key "<< i+1 << ":\t\t" << key << endl;
+
+			cout << "Message "<< i+1 << ":\t";
+			for ( int j=0; j < sizeof(encoded_data[i].message); j++) {
+				if ( encoded_data[i].message[j] == (char)0)
+					break;
+				cout << (char)(encoded_data[i].message[j]-encoded_data[i].key-j);
+			}
+			cout << endl << endl;
+		}
 	}
 	file.close();
-	
+
 	return 0;
 }
--- Encode.cc
+++ Encode.cc
@@ -13,38 +13,34 @@ struct Encoded {
 
 int main () {
 	int key;
-	Encoded encoded_data;	
+	Encoded encoded_data;
 	char input_buffer[BUFFER_SIZE];
-	
+
 	ofstream outfile("coded_message.txt", ios::out | ios::binary);
-	
+
 	if ( !outfile ) {
 		cerr << "Connot open file!" << endl;
 		return 2;
 	}
-	
+
 	cout << "Enter a positive integer for key: ";
 	cin >> key;
-	
+
 	while ( key > 0 ) {
 		cout << "Enter one line of message: ";
 		cin.ignore();
 		cin.getline(input_buffer, BUFFER_SIZE);
 		cerr << "HAVE MESSAGE: " << input_buffer << endl;
 		encoded_data.key = key;
-		
 		for ( int i = 0; i < strlen(input_buffer); i++ ) {
 			encoded_data.message[i] = i + key + input_buffer[i];
-		} 
-		
+		}
 		outfile.write( (char *) &encoded_data, sizeof(Encoded));
-		
 		cout << "Enter next key (0 to stop): ";
-		
 		cin >> key;
 		cerr << "HAVE key: " << key << endl << endl;
 	}
-	
+
 	outfile.close();
 	return 0;
 }
